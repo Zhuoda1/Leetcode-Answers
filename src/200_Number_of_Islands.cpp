833e5063ac5da4737ccde6ae025ab540a1a20fe1
Zhuoda <lzd020815@gmail.com>
2022-10-29 10:40:04 -0500

    (src) Answer 200

--- src/200_Number_of_Islands.cpp
+++ src/200_Number_of_Islands.cpp
@@ -37,43 +37,4 @@ public:
 
         return ans;
     }
-};class Solution {
-public:
-    int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
-        queue<pair<int,int>> q;
-
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == '1'){
-                    ans++;
-                    grid[i][j] = '0';
-                    q.push(make_pair(i,j));
-                    while(!q.empty()){
-                        int row = q.front().first;
-                        int column = q.front().second;
-                        q.pop();
-                        if(column >= 1 && grid[row][column-1] == '1'){
-                            q.push({row,column-1});
-                            grid[row][column-1] = '0';
-                        }
-                        if(row >= 1 && grid[row-1][column] == '1'){
-                            q.push({row-1,column});
-                            grid[row-1][column] = '0';
-                        }
-                        if(column < grid[0].size() - 1 && grid[row][column+1] == '1'){
-                            q.push({row,column+1});
-                            grid[row][column+1] = '0';
-                        }
-                        if(row < grid.size() - 1 && grid[row+1][column] == '1'){
-                            q.push({row+1,column});
-                            grid[row+1][column] = '0';
-                        }
-                    }
-                }
-            }
-        }
-
-        return ans;
-    }
 };
