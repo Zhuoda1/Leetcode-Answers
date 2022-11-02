1366fb18d41bf6f55068a7e9864610f24143fb4d
Zhuoda <lzd020815@gmail.com>
2022-11-02 10:30:22 -0500

    (src) Answer 74

--- src/74_Search_a_2D_Matrix.cpp
+++ src/74_Search_a_2D_Matrix.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        vector<int> first;
-        for(int i = 0; i < matrix.size(); i++){
-            first.push_back(matrix[i][0]);
-        }
-        int index = nearSearch(first, target);
-        if(first[index] == target) return true;
-        if(target <= matrix[index][matrix[index].size() - 1]) return biSearch(matrix[index], target);
-        else return false;
+        int index = nearSearch(matrix, target);
+        if(index < 0) return false;
+        return biSearch(matrix[index], target);
     }
     bool biSearch(vector<int> arr, int target){
         int left = 0, right = arr.size() - 1;
@@ -20,22 +15,17 @@ public:
         }
         return false;
     }
-    int nearSearch(vector<int> arr, int target){
-        int left = 0, right = arr.size() - 1;
-        while(left <= right){
-            int mid = (left + right) >> 1;
-            if(arr[mid] == target) return mid;
-            else if(arr[mid] < target){
-                if(mid < arr.size() - 1 && arr[mid+1] > target) return mid;
-                else if(mid == arr.size() - 1) return mid;
-                left = mid + 1;
+    int nearSearch(vector<vector<int>> arr, int target){
+        int left = -1, right = arr.size() - 1;
+        while(left < right){
+            int mid = (left + right + 1) >> 1;
+            if(arr[mid][0] <= target){
+                left = mid;
             }
             else{
-                if(mid > 0 && arr[mid-1] < target) return mid - 1;
-                else if(mid == 0) return mid;
                 right = mid - 1;
             }
         }
-        return (left + right) >> 1;
+        return left;
     }
 };
