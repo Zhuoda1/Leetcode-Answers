cd9dd0db25b61a6e337a9ded6c7ff5dd7d0878b3
Zhuoda <lzd020815@gmail.com>
2022-11-22 21:21:30 -0600

    (src) Answer 40

--- src/40_Combination_Sum_II.cpp
+++ src/40_Combination_Sum_II.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     vector<vector<int>> ans;
     vector<int> p;
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         dfs(candidates, target, 0);
         return ans;
     }
@@ -12,8 +13,9 @@ public:
         else if(target < 0) return;
 
         for(int i = start; i < candidates.size(); i++){
+            if(i != start && candidates[i] == candidates[i-1]) continue;
             p.push_back(candidates[i]);
-            dfs(candidates, target - candidates[i], i);
+            dfs(candidates, target - candidates[i], i + 1);
             p.pop_back();
         }
     }
