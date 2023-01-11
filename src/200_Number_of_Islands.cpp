b1e0b77cd839d7dafb005051411124c0890bb130
Zhuoda <lzd020815@gmail.com>
2023-01-11 15:39:27 -0600

    (src) Answer 200

--- src/200_Number_of_Islands.cpp
+++ src/200_Number_of_Islands.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        int dir[4][2] = {{-1,0},{0,-1},{0,1},{1,0}};
+        int cnt = 0;
         queue<pair<int,int>> q;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '0') continue;
+                cnt++;
+                grid[i][j] = '0';
+                q.push({i,j});
+                while(!q.empty()){
+                    int x = q.front().first;
+                    int y = q.front().second;
+                    q.pop();
 
-        for(int i = 0; i < grid.size(); i++){
-            for(int j = 0; j < grid[0].size(); j++){
-                if(grid[i][j] == '1'){
-                    ans++;
-                    grid[i][j] = '0';
-                    q.push(make_pair(i,j));
-                    while(!q.empty()){
-                        int row = q.front().first;
-                        int column = q.front().second;
-                        q.pop();
-                        if(column >= 1 && grid[row][column-1] == '1'){
-                            q.push({row,column-1});
-                            grid[row][column-1] = '0';
-                        }
-                        if(row >= 1 && grid[row-1][column] == '1'){
-                            q.push({row-1,column});
-                            grid[row-1][column] = '0';
-                        }
-                        if(column < grid[0].size() - 1 && grid[row][column+1] == '1'){
-                            q.push({row,column+1});
-                            grid[row][column+1] = '0';
-                        }
-                        if(row < grid.size() - 1 && grid[row+1][column] == '1'){
-                            q.push({row+1,column});
-                            grid[row+1][column] = '0';
+                    for(int p = 0; p < 4; p++){
+                        int nx = x + dir[p][0];
+                        int ny = y + dir[p][1];
+                        if(nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] == '1') {
+                            grid[nx][ny] = '0';
+                            q.push({nx,ny});
                         }
                     }
                 }
             }
         }
-
-        return ans;
+        return cnt;
     }
 };
