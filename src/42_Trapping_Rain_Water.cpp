ad98580cf2871077e80a17fd18d28830e2ffa369
Zhuoda <lzd020815@gmail.com>
2023-04-09 12:08:43 -0500

    (src) Answer 42

--- src/42_Trapping_Rain_Water.cpp
+++ src/42_Trapping_Rain_Water.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        // smaller than prev, push
+        // higher than prev, pop and count water between them, push new
         stack<int> st;
         int ans = 0;
-        int curr = 0;
-        while(curr < height.size()){
-            while(!st.empty() && height[curr] > height[st.top()]){
-                int h = height[st.top()];
+        st.push(0);
+        for(int i = 1; i < height.size(); i++){
+            while(!st.empty() && height[i] > height[st.top()]){
+                int prev = st.top();
                 st.pop();
-                if(st.empty()) break;
-                int dis = curr - st.top() - 1;
-                int m = min(height[curr], height[st.top()]);
-                ans += dis * (m - h);
+                if(!st.empty()) ans += (min(height[st.top()], height[i]) - height[prev]) * (i - st.top() - 1);
             }
-            st.push(curr);
-            curr++;
+            st.push(i);
         }
         return ans;
     }
