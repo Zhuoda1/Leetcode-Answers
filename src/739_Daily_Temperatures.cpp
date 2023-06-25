84260e105e3693a962467e893821122cc1b69ad5
Zhuoda <lzd020815@gmail.com>
2023-06-26 00:18:03 +0800

    (src) Answer 739, 853

---
+++ src/739_Daily_Temperatures.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int size = temperatures.size();
+        vector<int> ans(size,0);
+        stack<int> st;
+        
+        for(int i = size-1; i >= 0; i--){
+            // first
+            while(st.size() && temperatures[st.top()] <= temperatures[i]) st.pop();
+            if(!st.empty()) ans[i] = st.top() - i;
+            st.push(i);
+        }
+        return ans;
+    }
+};
---
+++ src/853_Car_Fleet.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        if(n == 0) return 0;
+        vector<pair<int,int>> pns;
+        for(int i = 0; i < n; i++){
+            pns.push_back({position[i], speed[i]});
+        }
+        sort(pns.begin(), pns.end(), [](const pair<int,int>& a, const pair<int,int>& b){
+            return a.first > b.first;
+        });
+        stack<float> st;
+        for(int i = 0; i < n; i++){
+            float time =  (target - pns[i].first) / float(pns[i].second);
+            // cout<<pns[i].first<<" "<<pns[i].second<<endl;
+            if(st.empty() || time > st.top()) st.push(time);
+            // if(target * pns[cur].second - pns[i].first * pns[cur].second - target * pns[i].second + pns[cur].first * pns[i].second > 0) st.push(i);
+        }
+        // while(st.size()) cout<<st.top()<<endl, st.pop();
+        return st.size();
+    }
+};
