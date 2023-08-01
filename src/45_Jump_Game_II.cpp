1f38b13edd0cf208008bd790cdeb14910eb6ee22
Zhuoda <lzd020815@gmail.com>
2023-08-01 22:12:20 +0800

    (src) Answer 45

--- src/45_Jump_Game_II.cpp
+++ src/45_Jump_Game_II.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int m = 0, len = nums.size(), temp = 0, step = 0;
-        for(int i = 0; i < len - 1; i++){
-            if(m >= i){
-                m = max(m, i + nums[i]);
-                if(i == temp){
-                    temp = m;
-                    step++;
-                }
+        int curEnd = 0, curFar = 0, step = 0;
+        for(int i = 0; i < nums.size() - 1; i++){
+            curFar = max(curFar, i + nums[i]);
+            if(i == curEnd){
+                step++;
+                curEnd = curFar;
             }
         }
         return step;
